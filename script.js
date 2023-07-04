64abbad092228ac508e17adf1eaa017849329317
GiesserCode <121457691+GiesserCode@users.noreply.github.com>
2023-07-04 19:42:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function onload() {
-    setTimeout(() => {
-        document.querySelector(".loader").style.display = "none";
-    }, 2600);
+function home() {
+    open("../index.html", "_self")
 }
 
-function sticky() {
-    open("./sticky/sticky.html", "_self")
-}
-
-function snap() {
-    open("./snapscroll/snap.html", "_self")
-}
-
-function carussel() {
-    open("./carussel/carussel.html", "_self")
-}
